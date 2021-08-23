dfd68fca375f344262b3b04f9236c17d32063825
dntjd7701 <dntjd7701@gmail.com>
2021-08-23 17:52:23 +0900

    #Dr.[ADD]:localStorage 저장

--- src/App/Navigation.js
+++ src/App/Navigation.js
@@ -96,7 +96,7 @@ function Navigation() {
                         </a>
                     </li>
                     <li>
-                        <a href="/#/" onClick={() => window.location.href = '/sign-in'}>
+                        <a href="/sign-in" onClick={() => localStorage.clear()}>
                             <i className="ti ti-power-off"></i>
                         </a>
                     </li>
