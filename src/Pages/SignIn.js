7c745f12dbb2cebaac2c86f0ae4ec28f31b74409
dntjd7701 <dntjd7701@gmail.com>
2021-09-29 16:07:50 +0900

    #Dr.[MOD]:Sign-Up Alert 수정

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from "react"
 import {ReactComponent as Logo} from '../assets/img/logo.svg'
 import {Alert} from "reactstrap";
-
 import * as config from "../config/config"
 import img from "../assets/img/covengers-logo.png"
 
