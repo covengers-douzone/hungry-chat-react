a8316876d994d58051a1038c7515f77ef1b9f76f
PDH <54939319+wow1186@users.noreply.github.com>
2021-10-01 10:44:26 +0900

    #Thor [moidfy] 코드 블럭 길게 나오는거 짧게 변경

--- src/App/Module/chatForm.js
+++ src/App/Module/chatForm.js
@@ -17,25 +17,21 @@ const chatMessageForm = (chat , index ) => {
     const chatNo = chat.no;
     chat.type === 'TEXT' && (contents = chat.contents)
     chat.type === 'MARKDOWN' && (contents =
-
         <ReactMarkdown
-
             children={chat.contents}
-            remarkPlugins={[remarkGfm]}
             id={chatNo}
             components={{
                 code({node, inline, className, children, ...props}) {
                     match = /language-(\w+)/.exec(className || '')
                     return !inline && match ? (
                         <SyntaxHighlighter
-                            children={String(children).replace(/\n$/, '')}
+                            children={String(children).replace(/\n$/, '').substr(0,100)+"\n...... 전체 코드 보기" }
                             style={dark}
                             language={match[1]}
                             PreTag="div"
                             {...props}
                         />
                     ) : (
-
                         <code className={className} {...props}>
                             {children}
                         </code>
