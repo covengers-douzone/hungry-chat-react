8d412c8a87a6a253b026d8f79207929c381a566c
dntjd7701 <dntjd7701@gmail.com>
2021-10-05 14:52:25 +0900

    #Dr.[MERGE]:merge

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,10 @@ export const SOCKET_IP = 'http://127.0.0.1';
 export const SOCKET_PORT = '9999';
 export const SOCKET_GAME_PORT = '5555';
 export const DEFAULT_PROFILE_IMG = "http://simpleicon.com/wp-content/uploads/account.png";
-export const URL = 'http://127.0.0.1:9999';
+// export const URL = 'http://127.0.0.1:9999';
+export const URL = " https://my-app.loca.lt";
+
+
 export const CHAT_LIMIT = 10
-// export const SPRING_URL = 'http://fe61-218-39-226-3.ngrok.io'
-export const SPRING_URL = 'http://localhost:8888'
+export const SPRING_URL = "http://8349-218-39-226-3.ngrok.io"
+// export const SPRING_URL = 'http://localhost:8888'
